e0b29977db1d181ac56ea7e6ecfe5b4e19272c41
Daniel-616 <daniel.tsutsumanov@gmail.com>
2025-01-19 14:24:29 +0100

    Update sendEmail.js

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -39,10 +39,10 @@ export default async function handler(req, res) {
         const { 'g-recaptcha-response': captchaToken, name, email, phone, message } = fields;
 
         const isHuman = await verifyRecaptcha(captchaToken);
-        console.log(isHuman);
         if (!isHuman) {
             return res.status(400).json({ error: 'reCAPTCHA validation failed.' });
-        }
+        } else {
+			return res.status(400).json({ isHuman: 'success!'});
 
         const attachments = [];
         if (files.files && Object.keys(files.files).length > 0) {
